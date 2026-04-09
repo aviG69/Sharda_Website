ad94cbcd0b09579326616124714500e26808512e
aviG69 <hellotakumi73@gmail.com>
2026-04-10 00:49:54 +0530

    home done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="nav">
-      <h2>Sharda Tutorial</h2>
+      {/* LEFT SIDE (LOGO CLICKABLE) */}
+      <div className="nav-left">
+        <Link 
+          to="/" 
+          style={{ display: "flex", alignItems: "center", gap: "10px", textDecoration: "none" }}
+        >
+          <img src="/logo.jpg" alt="logo" className="logo" />
+          <h2 style={{ margin: 0 }}>Sharda Tutorial</h2>
+        </Link>
+      </div>
 
+      {/* RIGHT SIDE (LINKS) */}
       <div>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/batches">Batches</NavLink>
